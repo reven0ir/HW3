672a633078041bbce3d1831252d9f38f43afc2ea
Mychailo Yashchuk <michail.yashchuck@gmail.com>
2023-02-18 16:58:53 +0200

    Create for.cs

---
+++ Main/for/for.cs
@@ -0,0 +1,24 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Default;
+
+double a = 0.0;  // початок відрізку
+double b;
+
+Console.WriteLine("За замовчуванням початок відрізку = 0.0\n");
+
+Console.WriteLine("Введіть кількість відрізків: ");
+int n = Convert.ToInt32(Console.ReadLine()); // число відрізків
+
+Console.WriteLine("Введіть кінець відрізку: ");
+b = Convert.ToDouble(Console.ReadLine()); // кінець відрізку
+
+double h = (b - a) / n;  // довжина кожного відрізку
+
+Console.WriteLine($"Довжина кожного відрізку: {h}");
+Console.Write($"Точки розбиття відрізку [{a}, {b}]: ");
+
+for (int i = 0; i <= n; i++)
+{
+    double x = a + i * h;
+    Console.Write($"\n{x}; ");
+}
+Console.WriteLine();
