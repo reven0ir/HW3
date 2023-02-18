d6d9bf197012644c972495e5bdb67ff1068ba2e8
Mychailo Yashchuk <michail.yashchuck@gmail.com>
2023-02-18 17:31:07 +0200

    Update for.cs

--- Main/for/for.cs
+++ Main/for/for.cs
@@ -6,17 +6,17 @@ double b;
 Console.WriteLine("За замовчуванням початок відрізку = 0.0\n");
 
 Console.WriteLine("Введіть кількість відрізків: ");
-int n = Convert.ToInt32(Console.ReadLine()); // число відрізків
+int c = Convert.ToInt32(Console.ReadLine()); // кількість відрізків
 
 Console.WriteLine("Введіть кінець відрізку: ");
 b = Convert.ToDouble(Console.ReadLine()); // кінець відрізку
 
-double h = (b - a) / n;  // довжина кожного відрізку
+double h = (b - a) / c;  // довжина кожного відрізку
 
 Console.WriteLine($"Довжина кожного відрізку: {h}");
 Console.Write($"Точки розбиття відрізку [{a}, {b}]: ");
 
-for (int i = 0; i <= n; i++)
+for (int i = 0; i <= c; i++)
 {
     double x = a + i * h;
     Console.Write($"\n{x}; ");
